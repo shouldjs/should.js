341a4882b79d5eb0544086d3659ff24a3f9febbe
Denis Bardadym <bardadymchik@gmail.com>
2015-02-05 21:58:05 +0300

    When nested errors happen, do not generate message

---
+++ test/perf.test.js
@@ -0,0 +1,18 @@
+var should = require('../');
+
+it('should optimize not.equal for complex objects', function() {
+  function mockComplexObject(prefix, depth, width) {
+    if (depth <= 0) {
+      return 'value-' + prefix;
+    }
+    var obj = {};
+    for (var i = 0; i < width; i++) {
+      obj[prefix + depth + '_' + i] = mockComplexObject(prefix, depth-1, width);
+    }
+    return obj;
+  }
+  var a = mockComplexObject('a', 8, 4);
+  var b = mockComplexObject('b', 8, 4);
+
+  a.should.be.not.equal(b); // It takes a while
+});
