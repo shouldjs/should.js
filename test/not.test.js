073717122cbe1275d6b8346c2d25b78c353ef333
Denis Bardadym <bardadymchik@gmail.com>
2016-12-10 13:35:27 +0300

    Add a bit hacky error to throw instead of full powered AssertionError

---
+++ test/not.test.js
@@ -0,0 +1,28 @@
+var should = require('../');
+var err = require('./util').err;
+
+
+var a = 10;
+var b = 11;
+
+describe('.not', function() {
+  it('should not throw when true', function() {
+    a.should.be.exactly(a);
+  });
+
+  it('should throw when false', function() {
+    err(function() {
+      a.should.be.exactly(b);
+    }, 'expected 10 to be 11');
+  });
+
+  it('should throw when not true (false negative case)', function() {
+    err(function() {
+      a.should.not.be.exactly(a);
+    }, 'expected 10 not to be 10 (false negative fail)');
+  });
+
+  it('should not throw when not false', function() {
+    a.should.not.be.exactly(b);
+  });
+});
